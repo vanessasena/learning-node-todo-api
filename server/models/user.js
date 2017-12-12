dee7e74840c37f9ed92314c43f7d601cb12e1c0d
Vanessa Sena <nessa.aline@gmail.com>
2017-12-11 21:24:20 -0300

    Add POST /users/login route

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
-const mongoose = require('mongoose');
-const validator = require('validator');
+const mongoose = require('mongoose')
+const validator = require('validator')
 const jwt = require('jsonwebtoken')
 const _ = require('lodash')
 const bcrypt = require('bcryptjs')
@@ -48,7 +48,7 @@ UserSchema.methods.generateAuthToken = function () {
   user.tokens.push({access, token})
 
   return user.save().then(() => {
-    return token;
+    return token
   })
 }
 
@@ -69,24 +69,42 @@ UserSchema.statics.findByToken = function (token) {
   })
 }
 
+UserSchema.statics.findByCredentials = function (email, password){
+  var User = this
+  return User.findOne({email}).then((user) => {
+
+    if (!user) {
+      return Promise.reject()
+    }
+    return new Promise((resolve, reject) => {
+
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user)
+        } else {
+          reject()
+        }
+      })
+    })
+
+  })
+}
+
 UserSchema.pre('save', function(next) {
   var user = this
 
   if (user.isModified('password')) {
-
     bcrypt.genSalt(10, (err, salt) => {
       bcrypt.hash(user.password, salt, (err, hash) => {
-        user.password = hash;
+        user.password = hash
         next()
       })
     })
-
-
   } else {
     next()
   }
 })
 
-var User = mongoose.model('User', UserSchema);
+var User = mongoose.model('User', UserSchema)
 
-module.exports = {User};
+module.exports = {User}
