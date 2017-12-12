66220b6a99ee7280e3088021fb78b44a725496b1
Vanessa Sena <nessa.aline@gmail.com>
2017-12-12 11:30:47 -0300

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -127,6 +127,14 @@ app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user)
 })
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+  req.user.removeToken(req.token).then(() => {
+    res.status(200).send()
+  }, () => {
+    res.status(400).send()
+  })
+})
+
 app.listen(PORT, () => {
   console.log(`Started on port ${PORT}`)
 })
